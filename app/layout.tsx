e520a154e8f1d74d8c49ac1ca53877dbfe6703ec
jayakrishna <jayakrishna@beyondscale.tech>
2026-02-03 22:26:57 +0530

    vercel analytics implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import Script from "next/script";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -45,6 +46,7 @@ export default function RootLayout({
         </Script>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
